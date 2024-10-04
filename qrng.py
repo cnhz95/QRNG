ce76158ace4437a7ffce8731f2b40594e327299e
Christian Hernandez <cnhz95@gmail.com>
2024-10-04 23:12:36 +0200

    Runs in real quantum computer now

--- qrng.py
+++ qrng.py
@@ -2,17 +2,20 @@ import os
 from dotenv import load_dotenv
 from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
 from qiskit import QuantumCircuit, transpile
-from qiskit_aer import Aer
+
+load_dotenv()
+service = QiskitRuntimeService(channel="ibm_quantum", token=os.getenv("API_KEY"))
+backend = service.least_busy(operational=True, simulator=False, min_num_qubits=2)
+print(backend.name)
 
 def qrng(qc, bits):
-	random_bits = []
-	simulator = Aer.get_backend('qasm_simulator')
+	random_number = 0
+	sampler = Sampler(backend)
 	for i in range(bits):
-		job = simulator.run(qc, shots=1)
-		random_bit = int(list(job.result().get_counts().keys())[0])
-		random_bits.append(random_bit)
-	random_number = int("".join(map(str, random_bits)), 2)
-	return random_bits, random_number
+		job = sampler.run([qc], shots=1)
+		random_bit = int(next(iter(job.result()[0].data.meas.get_counts())))
+		random_number = (random_number << 1) | random_bit
+	return random_number
 
 if __name__ == "__main__":
 	qc = QuantumCircuit(1)
@@ -20,4 +23,7 @@ if __name__ == "__main__":
 	qc.measure_all()
 	print(qc.draw())
 
-	print(f"Random number: {qrng(qc, bits=32)[1]}")
+	qc_transpiled = transpile(qc, backend)
+	print(qc_transpiled.draw())
+
+	print(f"Random number: {qrng(qc_transpiled, bits=32)}")
